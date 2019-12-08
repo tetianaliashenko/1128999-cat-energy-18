c54bfbf268ba5f0a1ff92e22d8d650190aae2673
Tetiana Liashenko <tetiana.liashenko@ukr.net>
2019-12-09 01:36:05 +0200

    Стилизует декоративные элементы страници с формами и вносит правки

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,5 +1,5 @@
 var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navToggle = document.querySelector('.page-header__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
